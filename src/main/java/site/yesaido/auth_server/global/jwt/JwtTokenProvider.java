e3e78314f6d779d7e61c0583d55b585051372e22
hetgwi01 <hetgwi03@gmail.com>
2026-07-27 20:38:48 +0900

    재웅님 import 잘못해서 컴파일 에러나요

--- src/main/java/site/yesaido/auth_server/global/jwt/JwtTokenProvider.java
+++ src/main/java/site/yesaido/auth_server/global/jwt/JwtTokenProvider.java
@@ -5,11 +5,11 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import site.yesaido.auth_server.domain.user.entity.Role;
 
-import javax.annotation.PostConstruct;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
