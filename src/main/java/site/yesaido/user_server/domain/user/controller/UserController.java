4a585718b992e12b6efc525b1dfde97415f09ea9
Poly-Etilen <wlsdud04060@naver.com>
2026-08-01 12:26:04 +0900

    fix: 통신문제 해결

--- src/main/java/site/yesaido/user_server/domain/user/controller/UserController.java
+++ src/main/java/site/yesaido/user_server/domain/user/controller/UserController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
 
