26774c4804e16deee417dc6072b2c47a06d893f8
JaeUng <rnrn4308428@gmail.com>
2026-08-04 20:37:15 +0900

    ci: Auth_server 워크플로우에 Redis 테스트 서비스 추가

--- src/test/java/site/yesaido/user_server/UserServerApplicationTests.java
+++ src/test/java/site/yesaido/user_server/UserServerApplicationTests.java
@@ -1,11 +1,11 @@
 package site.yesaido.user_server;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
-@ActiveProfiles("dev")
 class UserServerApplicationTests {
 
     @Test
