0325564f46b514d76491690efebbd85c444c73f1
JaeUng <rnrn4308428@gmail.com>
2026-08-04 20:27:19 +0900

    @ActiveProfiles("dev")

--- src/test/java/site/yesaido/user_server/UserServerApplicationTests.java
+++ src/test/java/site/yesaido/user_server/UserServerApplicationTests.java
@@ -2,9 +2,10 @@ package site.yesaido.user_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class UserServerApplicationTests {
 
     @Test
