93d1705501207c3d715eb2b36baa6ce97d50cbef
Poly-Etilen <wlsdud04060@naver.com>
2026-08-05 09:30:23 +0900

    fix: profile 설정

--- src/test/java/site/yesaido/user_server/UserServerApplicationTests.java
+++ src/test/java/site/yesaido/user_server/UserServerApplicationTests.java
@@ -3,9 +3,11 @@ package site.yesaido.user_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServerApplicationTests {
 
     @Test
