813e53d0342a5bc960d1ad7bc62d32075377e594
JaeUng <rnrn4308428@gmail.com>
2026-08-18 10:53:13 +0900

    test: SonarQube 규칙(S2699) 준수를 위해 UserServerApplicationTests에 assertion 추가

--- src/test/java/site/yesaido/user_server/UserServerApplicationTests.java
+++ src/test/java/site/yesaido/user_server/UserServerApplicationTests.java
@@ -1,17 +1,25 @@
 package site.yesaido.user_server;
 
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class UserServerApplicationTests {
 
+    @Autowired
+    private ApplicationContext context;
+
     @Test
+    @DisplayName("스프링 컨텍스트가 정상적으로 로드된다")
     void contextLoads() {
+        assertThat(context).isNotNull();
     }
 
 }
